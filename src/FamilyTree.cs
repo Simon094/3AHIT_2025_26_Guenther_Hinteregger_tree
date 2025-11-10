eaa63e3d30b78d04c313c01f641df594e72125c1
Simon094 <simon.kumpitsch@gmx.at>
2025-11-10 08:53:33 +0100

    Co-authored-by: Lukas Mayr <GimliTV323@users.noreply.github.com>

---
+++ src/FamilyTree.cs
@@ -0,0 +1,26 @@
+namespace FamilytreesLib;
+
+public class FamilyTree
+{
+    private List<Person> _person = new List<Person>();
+    private string _name;
+
+    public FamilyTree(string name, List<Person> person)
+    {
+        _name = name;
+        _person = person;
+    }
+    public string getName()
+    {
+        return _name;
+    }
+    public void AddPerson(Person person)
+    {
+        _person.Add(person);
+    }
+
+    public void RemovePerson(Person person)
+    {
+        _person.Remove(person);
+    }
+}
---
+++ src/Person.cs
@@ -0,0 +1,62 @@
+﻿using System.Drawing;
+using System.Windows;
+namespace FamilytreesLib;
+
+public class Person
+{
+  private string _name;
+  private DateTime _birthdate;
+  private DateTime _deathdate;
+  private bool _married;
+  private bool _isDead;
+  private List<Person> _children = new List<Person>();
+
+  public Person(string name, DateTime birthdate, bool married)
+  {
+    _name = name;
+    _birthdate = birthdate;
+    _married = married;
+
+  }
+
+  public void addChild(Person child)
+  {
+    _children.Add(child);
+  }
+
+  public void addDeathdate(DateTime date)
+  {
+    _deathdate = date;
+  }
+
+  public void changeMariageStatus()
+  {
+    if (_married == false)
+    {
+      _married = true;
+    }
+    else if (_married == true)
+    {
+      _married = false;
+    }
+    else
+    {
+      throw new Exception("Keine Ahnung Bruder");
+    }
+  }
+
+  public string getName()
+    {
+        return _name;
+    }
+
+  public DateTime getBirthdate()
+    {
+      return _birthdate;  
+    }
+
+  public DateTime getDeathdate()
+    {
+      return _deathdate;
+    }
+}
