f5178aa5bc8794102206b1be29a600540e898709
Lukas Mayr <lukasmayr08@gmail.com>
2025-12-05 09:41:31 +0100

    managed the database

--- Program/insertDatabank.sql
+++ Program/insertDatabank.sql
@@ -1,7 +1,7 @@
 INSERT OR IGNORE INTO Person
 (Id, Name, Birthdate, Deathdate, IsMarried, IsMale, Job, School, FatherId, MotherId, Image)
 VALUES
-(67667, 'Günther Hinteregger', '1970-10-09', NULL, 1, 1, 'Arzt', 'HTL-Weiz', 67, NULL, NULL);
+(7, 'Günther Hinteregger', '1970-10-09', NULL, 1, 1, 'Arzt', 'HTL-Weiz', 67, NULL, NULL);
 
 INSERT OR IGNORE INTO FamilyTree
 (Name)
--- Program/sourceDatabank.sql
+++ Program/sourceDatabank.sql
@@ -1,15 +1,12 @@
 CREATE TABLE IF NOT EXISTS Person (
     Id INTEGER PRIMARY KEY AUTOINCREMENT,
     Name TEXT NOT NULL,
-    Birthdate TEXT NOT NULL,
-    Deathdate TEXT NULL,
+    Birthdyear TEXT NOT NULL,
+    Deathyear TEXT NULL,
     IsMarried INTEGER NOT NULL,
     IsMale INTEGER NOT NULL,
-    Job TEXT,
-    School TEXT,
     FatherId INTEGER NULL,
     MotherId INTEGER NULL,
-    Image BLOB,
     FOREIGN KEY (FatherId) REFERENCES Person(Id),
     FOREIGN KEY (MotherId) REFERENCES Person(Id)
 );
